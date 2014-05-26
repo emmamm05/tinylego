f88449da5f4fb42cbe2ea069b3b33c21ba8972f2
emmamm05 <emmanuelmora05@gmail.com>
2014-05-26 09:59:53 -0600

    Index

--- www/js/backend-conexion.js
+++ www/js/backend-conexion.js
@@ -1,28 +1,33 @@
-
-
-var Parse = {
-   this.READ = function() {  
-      var Figura = Parse.Object.extend("mini_figura");
-      var query = new Parse.Query(Figura);
-      query.get("s20Gqz766g", {
-	success: function(Figura) {
-	  alert("Successfully retrieved " + results.length + " scores.");
-	  // Do something with the returned Parse.Object values
-	  for (var i = 0; i < results.length; i++) { 
-	    var object = results[i];
-	    alert(object.id + ' - ' + object.get('playerName'));
-	  }
+Parse.initialize("B0RmWvAnwf8prJLztD9wSGsxAn1o2Lknzl6tqyGg", "Uca15YO3L0TNyNfZYwfC9ENCVm6dIY1U8AkDj0LN");
+var Figura = Parse.Object.extend("mini_figura");
+var query = new Parse.Query(Figura);	    
+query.find({
+  success: function(results) {
+      for (var i = 0; i < results.length; i++) { 
+	var object = results[i];
+	var url = object.get('foto').url() ;
+	var title = object.get('nombre');
+	var serie = object.get('serie');
+	var descripcion = object.get('descripcion');
+	
+	//Para el carousel
+	document.getElementById('links').innerHTML = document.getElementById('links').innerHTML + 
+	  "<a href='"+ url +"' title='"+title+"' data-gallery></a>";
 	  
-	  self.$(".container.text").html("Holas").show();
-	  
-	  return Figura.get("nombre");
-	},
-	error: function(object, error) {
-	  alert("Error: " + error.code + " " + error.message);
-	  // The object was not retrieved successfully.
-	  // error is a Parse.Error with an error code and description.
-	}
-      });
+	//Para el index
+	document.getElementById("minifigures").innerHTML = document.getElementById("minifigures").innerHTML + 
+	  "<div class='row'>" +
+	    "<div class='col-md-4'>"+"<img src='"+url+"'>"+"</div>" +
+	    "<div class='col-md-4'>" +
+	      "<div class='row'>"+"<a href='"+url+"' data-gallery>"+title+"</a></div>" + 
+	      "<div class='row'>"+serie+"</div>" + 
+	      "<div class='row'>"+descripcion+"</div>" + 
+	    "</div>" +
+	  "</div>";
+	
+      }
+  },
+  error: function(error) {
+    alert("Se econtraron con error");
   }
-
-}
+});
