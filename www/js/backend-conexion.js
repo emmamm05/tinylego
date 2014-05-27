92882d4c9d86401964a95a16306a73eee3663cf6
emmamm05 <emmanuelmora05@gmail.com>
2014-05-27 17:49:51 -0600

    Reacomodo

--- www/js/backend-conexion.js
+++ www/js/backend-conexion.js
@@ -5,27 +5,38 @@ query.find({
   success: function(results) {
       for (var i = 0; i < results.length; i++) { 
 	var object = results[i];
-	var url = object.get('foto').url() ;
+	var url_src = object.get('foto') ;
 	var title = object.get('nombre');
 	var serie = object.get('serie');
 	var descripcion = object.get('descripcion');
-	
-	//Para el carousel
-	document.getElementById('links').innerHTML = document.getElementById('links').innerHTML + 
-	  "<a href='"+ url +"' title='"+title+"' data-gallery></a>";
+	console.log(object);
 	  
 	//Para el index
-	document.getElementById("minifigures").innerHTML = document.getElementById("minifigures").innerHTML + 
+	if (!url_src){
+	  document.getElementById("minifigures").innerHTML = document.getElementById("minifigures").innerHTML + 
 	  "<div class='row'>" +
-	    "<div class='col-md-4'>"+"<img src='"+url+"'>"+"</div>" +
 	    "<div class='col-md-4'>" +
-	      "<div class='row'>"+"<a href='"+url+"' data-gallery>"+title+"</a></div>" + 
+	      "<div class='row'>"+title+"</div>" + 
 	      "<div class='row'>"+serie+"</div>" + 
 	      "<div class='row'>"+descripcion+"</div>" + 
 	    "</div>" +
-	  "</div>";
-	
-      }
+	  "</div>";	
+        }else{	
+	  //Para el carousel
+	  url = url_src.url();
+	  document.getElementById('links').innerHTML = document.getElementById('links').innerHTML + 
+	  "<a href='"+ url +"' title='"+title+"' data-gallery></a>";
+	  document.getElementById("minifigures").innerHTML = document.getElementById("minifigures").innerHTML + 
+	    "<div class='row'>" +
+	      "<div class='col-md-4'>"+"<img src='"+url+"'>"+"</div>" +
+	      "<div class='col-md-4'>" +
+		"<div class='row'>"+"<a href='"+url+"' data-gallery>"+title+"</a></div>" + 
+		"<div class='row'>"+serie+"</div>" + 
+		"<div class='row'>"+descripcion+"</div>" + 
+	      "</div>" +
+	    "</div>";
+	};
+      };
   },
   error: function(error) {
     alert("Se econtraron con error");
