d75bbc26cf570b2393eeded85799c573ec7d32df
emmamm05 <emmanuelmora05@gmail.com>
2014-05-27 19:10:55 -0600

    Camara

---
+++ platforms/android/ant-build/TinyLego-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/home/emma/projects/tinylego/platforms/android/ant-build/TinyLego-debug-unaligned.apk : \
+/home/emma/projects/tinylego/platforms/android/ant-build/TinyLego.ap_ \
+/home/emma/projects/tinylego/platforms/android/ant-build/classes.dex \
+/home/emma/projects/tinylego/platforms/android/CordovaLib/ant-build/classes.jar \
--- platforms/android/ant-build/classes.dex.d
+++ platforms/android/ant-build/classes.dex.d
@@ -1,9 +1,12 @@
 /home/emma/projects/tinylego/platforms/android/ant-build/classes.dex : \
 /home/emma/projects/tinylego/platforms/android/ant-build/classes/cr/tec/moviles/R$attr.class \
-/home/emma/projects/tinylego/platforms/android/ant-build/classes/cr/tec/moviles/App.class \
 /home/emma/projects/tinylego/platforms/android/ant-build/classes/cr/tec/moviles/R$string.class \
+/home/emma/projects/tinylego/platforms/android/ant-build/classes/cr/tec/moviles/TinyLego.class \
 /home/emma/projects/tinylego/platforms/android/ant-build/classes/cr/tec/moviles/R$drawable.class \
 /home/emma/projects/tinylego/platforms/android/ant-build/classes/cr/tec/moviles/BuildConfig.class \
 /home/emma/projects/tinylego/platforms/android/ant-build/classes/cr/tec/moviles/R$xml.class \
 /home/emma/projects/tinylego/platforms/android/ant-build/classes/cr/tec/moviles/R.class \
+/home/emma/projects/tinylego/platforms/android/ant-build/classes/org/apache/cordova/camera/CameraLauncher.class \
+/home/emma/projects/tinylego/platforms/android/ant-build/classes/org/apache/cordova/camera/ExifHelper.class \
+/home/emma/projects/tinylego/platforms/android/ant-build/classes/org/apache/cordova/camera/FileHelper.class \
 /home/emma/projects/tinylego/platforms/android/ant-build/dexedLibs/classes-8f62dc8b58151cbde8d82d9205e4270f.jar \
