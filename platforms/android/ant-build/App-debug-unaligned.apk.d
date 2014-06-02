395f96e7cd26652e90eb5f9f7f6b970319aa7236
emmamm05 <emmanuelmora05@gmail.com>
2014-06-01 20:47:06 -0600

    busqueda de etiquetas incompleta

--- platforms/android/ant-build/App-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/emma/projects/tinylego/platforms/android/ant-build/App-debug-unaligned.apk : \
-/home/emma/projects/tinylego/platforms/android/ant-build/App.ap_ \
-/home/emma/projects/tinylego/platforms/android/ant-build/classes.dex \
-/home/emma/projects/tinylego/platforms/android/CordovaLib/ant-build/classes.jar \
--- www/js/backend-conexion.js
+++ www/js/backend-conexion.js
@@ -4,7 +4,14 @@ function loadData(){
 	var Figura = Parse.Object.extend("mini_figura");
 	var query = new Parse.Query(Figura);	    
 	query.find({
-	  success: function(results) {
+	      success: renderResults,
+		  error: function(error) {
+		    alert("Se econtraron con error");
+		  }
+	});
+};
+
+function renderResults(results){
 	  	  document.getElementById('links').innerHTML = "";
 	  	  document.getElementById("minifigures").innerHTML = "";
 	      for (var i = 0; i < results.length; i++) { 
@@ -41,9 +48,116 @@ function loadData(){
 			    "</div>";
 			};
 		      };
-		  },
-		  error: function(error) {
-		    alert("Se econtraron con error");
-		  }
-	});
-}
+		  };
+
+function salvarFigura() {
+	var parseImage = new Parse.File("foto.img",{base64:image});
+	parseImage.save().then(
+		function() {
+			var Figura = Parse.Object.extend("mini_figura");
+			var figura = new Figura();
+			//Figuras
+			figura.set("nombre", document.getElementById("inputTitulo").value);
+			figura.set("descripcion", document.getElementById("inputDescripcion").value);
+			figura.set("serie", document.getElementById("inputSerie").value);
+			figura.set("foto", parseImage);
+			figura.save(null, {
+				success: function(figura) {
+					alert('New figura created with objectId: ' + figura.id);	
+					//Etiquetas
+					var etiquetasString = document.getElementById("inputEtiquetas").value.split(',');
+					for (var i = 0; i < etiquetasString.length; i++) { 		 
+					  	var Etiqueta = Parse.Object.extend("etiqueta");
+					  	var etiqueta = new Etiqueta();	
+					  	etiqueta.set("nombre",etiquetasString[i]);	      
+						//EtiquetasFiguras
+						var etiquetasString = document.getElementById("inputEtiquetas").value.split(',');	 
+						var EtiquetaFigura = Parse.Object.extend("etiqueta_figura");
+						var etiqueta_figura = new EtiquetaFigura();	
+						etiqueta_figura.set("etiqueta",etiqueta);	
+						etiqueta_figura.set("figura",figura);
+						etiqueta_figura.save(null, {
+							success: function(etiqueta_figura) {
+							    // Execute any logic that should take place after the object is saved.
+							    alert(' New etiqueta_figura created with objectId: ' + etiqueta_figura.id );
+							    loadData();
+							},
+							error: function(etiqueta_figura, error) {
+							    // Execute any logic that should take place if the save fails.
+							    // error is a Parse.Error with an error code and description.
+							    alert('Failed to create new etiqueta, with error code: ' + error.description);
+							}
+						});
+						etiqueta.save(null, {
+							success: function(etiqueta) {
+						    	// Execute any logic that should take place after the object is saved.
+						    	alert('New etiqueta created with objectId: ' + etiqueta.id );
+							},
+							error: function(etiqueta, error) {
+								// Execute any logic that should take place if the save fails.
+								// error is a Parse.Error with an error code and description.
+								alert('Failed to create new etiqueta, with error code: ' + error.description);
+							}
+						});	
+					};
+				},
+				error: function(figura, error) {
+					alert('Failed to create new figura, with error code: ' + error.description);
+				}
+			});
+		    // The file has been saved to Parse.
+		    alert("Imagen Cargada en Parse");
+	    }, function(error) {
+	        // The file either could not be read, or could not be saved to Parse.
+	        alert("Error subiendo imagen: "+error);
+	    });
+};
+
+
+function buscarPorEtiqueta(){
+	var tags = document.getElementById("inputBusquedaTags").value.split(",");
+	var figurasEncontradas = [];
+	for (var indextag=0; indextag < tags.length; indextag++){
+		//busqueda de las etiquetas
+		var Etiqueta = Parse.Object.extend("etiqueta");
+		var query = new Parse.Query(Etiqueta);
+		console.log("query: " + query+" , etiquetas: "+tags);
+		query.equalTo("etiqueta",tags[indextag]);
+		query.find({
+			success: function(etiquetas){
+				console.log("etiquetas: "+etiquetas);
+				var EtiquetaFigura = Parse.Object.extend("etiqueta_figura");
+				var query = new Parse.Query(EtiquetaFigura);
+				query.equalTo("etiqueta",etiquetas[0]);
+				query.find({
+					success: function(etiquetas_figuras){
+						console.log("etiquetas_figuras: "+etiquetas_figuras);
+						for (var et_fig_index=0; 
+							et_fig_index<etiquetas_figuras.length; et_fig_index++){
+							var Figura = Parse.Object.extend("figura");
+							var query = new Parse.Query(Figura);
+							query.equalTo("figura",etiquetas_figuras[0]);
+							query.find({
+								success: function(figura){
+									figurasEncontradas.push(figura);
+								},
+								error: function(error){
+									console.log("figura "+error);
+								}
+							});
+							
+						}
+					}, 
+					error: function(error){
+						console.log("etiqueta_figura "+error);
+					}
+				});
+			},
+			error: function(error){
+				console.log("etiqueta: "+error);
+			}
+		});
+		console.log("figuras: "+figurasEncontradas);
+		renderResults(figurasEncontradas);
+	};
+};
